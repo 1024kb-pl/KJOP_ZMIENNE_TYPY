b89b2fcea441ff4089c8a166cf8832a64821c4f5
klimson <klimson@gitlab.com>
2018-08-12 07:56:51 +0200

    init commit

---
+++ src/pl/maniaq/Zadanie1.java
@@ -0,0 +1,14 @@
+package pl.maniaq;
+
+public class Zadanie1 {
+
+    public static void main(String[] args) {
+        int age = 34;
+        long value = 3000423111L;
+        float pi = 3.1415F;
+        double e = 2.7182818284590d;
+        boolean isMale = true;
+        short weight = 85;
+        char letterA = 'a';
+    }
+}
---
+++ src/pl/maniaq/Zadanie2.java
@@ -0,0 +1,17 @@
+package pl.maniaq;
+
+public class Zadanie2 {
+
+    public static void main(String[] args) {
+        int age = 35;
+        float floatNumber = 43.4f;
+        char letterA = 'a';
+        boolean isTrue = true;
+        char letterB = 'b';
+        boolean isFalse = false;
+        float anotherFloatNumber = 95.4f;
+        long longNumber = 30120320323l;
+        double doubleNumber = 2.8123123123;
+        int anyInt = 33;
+    }
+}
